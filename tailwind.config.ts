adcc3ed314b9ac1bd1983e542d10541d9100d5b9
aaveshkhan882411-gif <aaveshkhan.882411@gmail.com>
2026-09-01 00:32:16 +0530

    Convert tailwind.config.js to TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
@@ -12,3 +12,4 @@ module.exports = {
   plugins: [],
 };
 
+export default config;
